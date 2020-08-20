cbcc5b3637b4fe21913d7afeaaed25ef68b1b01a
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-20 01:29:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    10.times do "s" + string end
+    10 * "s" + string
   else
     string
   end
