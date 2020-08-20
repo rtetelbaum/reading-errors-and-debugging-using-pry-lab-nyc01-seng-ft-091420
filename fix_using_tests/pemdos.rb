afa9806276f28ebcdc91928bd492ec150aa5bb14
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-20 01:27:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,8 @@
 # don't forget to add: require 'pry'
 
-string = "surprise!"
 def snake_it_up(string)
   if string[0] == "s"
-    10.times do
-      "s" + string
-    end
+    10 * "s" + string
   else
     string
   end
